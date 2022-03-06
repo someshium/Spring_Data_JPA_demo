1466d76593dfc59e00085b029a81dd672afcdb95
someshium <someshjdh2018@gmail.com>
2022-03-06 19:20:20 +0530

    updated the postgres database

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -11,6 +13,13 @@ public class DemoApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(DemoApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student somesh = new Student("Somesh", "Mishra", "somesh.mishra@google.com", 21);
+            studentRepository.save(somesh);
+        };
     }
 
 
---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,92 @@
+package com.example.demo;
+
+import javax.persistence.*;
+
+@Entity(name="Student")
+public class Student {
+
+    @Id
+    @SequenceGenerator( name="student_sequence", sequenceName = "student_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name="first_name", nullable = false, columnDefinition = "TEXT")
+    private String firstName;
+
+    @Column(name="last_name", nullable=false, columnDefinition = "TEXT")
+    private String lastName;
+
+    @Column(name="email", nullable = false,  columnDefinition = "TEXT", unique = true)
+    private String email;
+
+    @Column(name = "age", nullable = false)
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(
+                   String firstName,
+                   String lastName,
+                   String email,
+                   int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getfirstName() {
+        return firstName;
+    }
+
+    public void setfirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getlastName() {
+        return lastName;
+    }
+
+    public void setlastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
